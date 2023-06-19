d221cf9c4c2185a4969253f4f901d6b98720c1d0
john <johnmaluki58@gmail.com>
2023-06-19 19:56:23 +0300

    Complete assignment on array filter

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+const findMatching = (drivers, name) => {
+  return drivers.filter(
+    (driver_name) => driver_name.toUpperCase() === name.toUpperCase()
+  );
+};
+
+const fuzzyMatch = (drivers, name) => {
+  return drivers.filter(
+    (driver_name) => driver_name.slice(0, name.length) === name
+  );
+};
+
+const matchName = (drivers, name) => {
+  return drivers.filter((driver_name) => driver_name.name === name);
+};
